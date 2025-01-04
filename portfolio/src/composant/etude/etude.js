1cab541597f96f01b7fa9885f8e57ae8586a039d
BMD0910 <bmd09102000@gmail.com>
2025-01-04 15:28:28 +0000

    depot de mon protfolio

---
+++ portfolio/src/composant/etude/etude.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './etude.css';
+import Titre from '../titre/titre';
+
+
+
+
+const Etude = () => {
+      
+      return (
+        <div className="blog-etude">
+            <Titre  message='Etude' className='titre1'></Titre>
+            <ul className='etudes'>
+                <li className='etude'>
+                   <span className='entete'><span className='point'></span><span className='date-etude'>En cour</span></span>
+                   <span className='titre-etude'>Master en Système d'Information</span>
+                   <span className='cotenue-etude'>Université Alione Diop de Bambey</span>
+                </li>
+                <li className='etude'>
+                   <span className='entete'><span className='point'></span><span className='date-etude'>2021-2024</span></span> 
+                   <span className='titre-etude'>Licence en ingenieurie informatique</span>
+                   <span className='cotenue-etude'>Université Assane Seck de ziguinchor</span>
+                </li>
+                <li className='etude'>
+                   <span className='entete'><span className='point'></span><span className='date-etude'>2019-2020</span></span>
+                   <span className='titre-etude'>Bacalaurea serie s2</span>
+                   <span className='cotenue-etude'>Lycée seydina limamoulaye</span>
+                </li> 
+            </ul>
+        </div>
+      );
+}
+
+export default Etude;
